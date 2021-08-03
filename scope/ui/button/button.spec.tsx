349f6cea1aac1b4a45b0aa1010fc68a37e8afbe2
Rafik <rafiksargsyan07@gmail.com>
2021-08-03 11:10:02 +0400

    Update button.spec.tsx

--- scope/ui/button/button.spec.tsx
+++ scope/ui/button/button.spec.tsx
@@ -4,6 +4,6 @@ import { BasicButton } from './button.composition';
 
 it('should render with the correct text', () => {
   const { getByText } = render(<BasicButton />);
-  const rendered = getByText('Byby from Button');
+  const rendered = getByText('Foo bar');
   expect(rendered).toBeTruthy();
 });
